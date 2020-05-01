5e44d0de7d2f343546abd245694a6f3a71cb8060
Armin Biere <biere@jku.at>
2020-05-01 19:02:48 +0200

    got it

--- aws-run.sh
+++ aws-run.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-[ -f cnf ] && cnf=cnf
-exec lingeling/plingeling $cnf
+if [[ "${COMP_S3_PROBLEM_PATH}" == *".xz" ]]; then
+  aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/test.cnf.xz
+  unxz supervised-scripts/test.cnf.xz
+else
+  aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/test.cnf
+fi
+/lingeling/plingeling supervised-scripts/test.cnf -t ${NUM_PROCESSES}
